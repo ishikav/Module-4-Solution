9bfe4dc0ff95bd96a5e13fd425fe8dd56077d897
ishikav <65843008+ishikav@users.noreply.github.com>
2020-06-19 20:34:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var names=["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
+for (var name in names){
+	if (names[name].startsWith("J")||names[name].startsWith("j")){
+		console.log("Good Bye "+names[name]);
+	}
+	else{
+		console.log("Hello "+names[name]);
+	}
+}
